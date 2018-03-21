2ea742470df75d66bf6418df9c6e8bfefc9394ac
burobotics <burobotics>
2018-03-20 21:28:35 -0400

    initial commit

---
+++ dotemacsdotd/elpa/bracketed-paste-20160407.1648/bracketed-paste-pkg.el
@@ -0,0 +1,1 @@
+(define-package "bracketed-paste" "20160407.1648" "bracketed paste mode support within emacs -nw" '((emacs "24.3")) :commit "843ce3bbb63d560face889e13a57a2f7543957d5" :keywords '("terminals"))
