b4b9436e4c10ea5c39bd82b99258e37963c7e215
Xiao Li <xiaoli@isee.ai>
2018-06-06 13:17:19 -0400

    update

--- dotemacsdotd/elpa/bracketed-paste-20160407.1648/bracketed-paste-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; bracketed-paste-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "bracketed-paste" "bracketed-paste.el" (23151
-;;;;;;  30209 755780 375000))
-;;; Generated autoloads from bracketed-paste.el
-
-(autoload 'bracketed-paste-enable "bracketed-paste" "\
-Enable bracketed paste mode support on the terminal.
-
-\(fn)" nil nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; bracketed-paste-autoloads.el ends here
--- dotemacsdotd/elpa/bracketed-paste-20160407.1648/bracketed-paste-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "bracketed-paste" "20160407.1648" "bracketed paste mode support within emacs -nw" '((emacs "24.3")) :commit "843ce3bbb63d560face889e13a57a2f7543957d5" :keywords '("terminals"))
